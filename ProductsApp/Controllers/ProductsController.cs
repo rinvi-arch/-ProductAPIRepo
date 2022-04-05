5ef88a6207acd17257b0bb0246c48ddbe81a1b09
rinvi <mosammatr51@gmail.com>
2022-04-05 15:28:17 -0400

    Updating product varible

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -11,10 +11,11 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Schmoceries", Price = 42 },
+            new Product { Id = 2, Name = "iPhone 13 Pro Max", Category = "Electronic Device", Price = 1399.00M },
+            new Product { Id = 3, Name = "Cactus", Category = "Garden", Price = 13.98M }
         };
+
 
         //Added this from lab instruction
         public ProductsController()
